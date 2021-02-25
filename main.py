e1e2fc38507e80aa5d38acb3066531b3200a5f78
Sakshyam <sakshyam1991@gmail.com>
2021-02-25 11:20:42 +0000

    Results

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ from plots import plot, pv_total
 
 
 # PDF generalised function
-def generelised_PDF(atk_phase,lambda_list,rho,k,u_dist):
+def generalised_PDF(atk_phase,lambda_list,rho,k,u_dist):
     temp_lambda = np.zeros(shape=(atk_phase,atk_phase))
     temp_PDF = np.zeros(shape=(atk_phase,len(u_dist)))
     for i in range(atk_phase):
         for j in range(atk_phase):
             if j == i:
+                # print(f'{i}; j-->{j}')
                 temp_lambda[i][j] = lambda_list[i]
             else:
                 temp_lambda[i][j] = float(lambda_list[j]/(lambda_list[j]-lambda_list[i]))
@@ -26,6 +27,23 @@ def generelised_PDF(atk_phase,lambda_list,rho,k,u_dist):
     PDF = np.sum(temp_PDF, axis=0)
     return(PDF)
 
+# CDF generalised function
+def generalised_CDF(atk_phase,lambda_list,rho,k,u_dist):
+    temp_lambda = np.zeros(shape=(atk_phase,atk_phase))
+    temp_CDF = np.zeros(shape=(atk_phase,len(u_dist)))
+    for i in range(atk_phase):
+        for j in range(atk_phase):
+            if j == i:
+                temp_lambda[i][j] = 1
+            else:
+                temp_lambda[i][j] = float(lambda_list[j]/(lambda_list[j]-lambda_list[i]))
+
+    lambda_product = np.multiply.reduce(temp_lambda, axis=1)
+    for i in range(atk_phase):
+        temp_CDF[i] = [(j/k)**(lambda_list[i]/rho) * lambda_product[i] for j in u_dist]
+
+    CDF = np.sum(temp_CDF, axis=0)
+    return(CDF)
 
 ##### Parameter Values
 # Discount factor.
@@ -59,10 +77,10 @@ v_2 = np.arange(0.0, k_2, 0.1)
 v_3 = np.arange(0.0, k_3, 0.1)
 
 
-
 # Distribution of PV for phase 1 attack
 lambda_list_1 = [lambda_1]
-pv_anPDF_1 = generelised_PDF(1,lambda_list_1,rho,k_1,v_1)
+pv_anPDF_1 = generalised_PDF(1,lambda_list_1,rho,k_1,v_1)
+pv_anCDF_1 = generalised_CDF(1,lambda_list_1,rho,k_1,v_1)
 
 # Sanity check: integral pf PDF equal to 1
 f = lambda x:(lambda_1/rho * k_1**(-lambda_1/rho) * x**(lambda_1/rho-1))
@@ -73,7 +91,7 @@ print(f'f1--{sum(tem)}')
 # expression of the PDF and CDF, so we should also produce the simulated version.
 
 tau_1 = np.random.exponential(1/lambda_1,100000)           # sampling tau from an exponential distribution
-tau_11 = lognorm.rvs(0.5,1,100000)                          # sampling tau from an log-normal distribution
+tau_11 = np.random.lognormal(0.5,1,100000)                          # sampling tau from an log-normal distribution
 pv_emp_1 = k_1 * np.exp(-rho*tau_1)
 pv_emp_11 = k_1 * np.exp(-rho*tau_11)
 print(f'e1: {pv_emp_1}')
@@ -83,7 +101,8 @@ print(f'e11: {pv_emp_11}')
 
 # Distribution of PV for phase 2 attack
 lambda_list_2 = [lambda_1, lambda_2]
-pv_anPDF_2 = generelised_PDF(2,lambda_list_2,rho,k_2,v_2)
+pv_anPDF_2 = generalised_PDF(2,lambda_list_2,rho,k_2,v_2)
+pv_anCDF_2 = generalised_CDF(2,lambda_list_2,rho,k_2,v_2)
 
 # Sanity check: integral pf PDF equal to 2
 f = lambda x:(lambda_1*lambda_2/(lambda_2-lambda_1)*1/(k_2*rho)*((x/k_2)**(lambda_1/rho -1)-(x/k_2)**(lambda_2/rho - 1)))
@@ -91,17 +110,18 @@ tem = scipy.integrate.quad(f,0,k_2)
 print(f'f2--{sum(tem)}')
 
 tau_2 = np.random.exponential(1/lambda_2,100000)           # sampling tau from an exponential distribution
-tau_21 = lognorm.rvs(0.25,1.5,100000)                          # sampling tau from an log-normal distribution
+tau_21 = np.random.lognormal(0.25,1.5,100000)                          # sampling tau from an log-normal distribution
 pv_emp_2 = k_2 * np.exp(-rho*(tau_1+tau_2))
 pv_emp_21 = k_2 * np.exp(-rho*(tau_11+tau_21))
 print(f'e2: {pv_emp_2}')
-print(f'e21: {pv_emp_21}')
+print(f'e21: {pv_emp_21}, {tau_21}')
 
 
 
 #Distribution of PV for phase 3 attack
 lambda_list_3 = [lambda_1, lambda_2, lambda_3]
-pv_anPDF_3 = generelised_PDF(3,lambda_list_3,rho,k_3,v_3)
+pv_anPDF_3 = generalised_PDF(3,lambda_list_3,rho,k_3,v_3)
+pv_anCDF_3 = generalised_CDF(3,lambda_list_3,rho,k_3,v_3)
 
 # Sanity check: integral pf PDF equal to 3
 prod_lambda_3 = lambda_1 * lambda_2 * lambda_3
@@ -113,7 +133,7 @@ print(f'f3--{sum(tem)}')
 
 
 tau_3 = np.random.exponential(1/lambda_3,100000)           # sampling tau from an exponential distribution
-tau_31 = lognorm.rvs(1,0.75,100000)                          # sampling tau from an log-normal distribution
+tau_31 = np.random.lognormal(1,0.75,100000)                          # sampling tau from an log-normal distribution
 pv_emp_3 = k_2 * np.exp(-rho*(tau_1+tau_2+tau_3))
 pv_emp_31 = k_2 * np.exp(-rho*(tau_11+tau_21+tau_31))
 print(f'e3: {pv_emp_3}')
@@ -126,13 +146,13 @@ pv_2 = pv_emp_11 + pv_emp_21 + pv_emp_31
 
 # Plotting the results
 # plots for phase 1
-plot(v_1,pv_anPDF_1,pv_emp_1,pv_emp_11,phase='Phase 1')
+plot(v_1,pv_anPDF_1,pv_anCDF_1,pv_emp_1,pv_emp_11,phase='Phase 1')
 
 # plots for phase 2
-plot(v_2,pv_anPDF_2,pv_emp_2,pv_emp_21,phase='Phase 2')
+plot(v_2,pv_anPDF_2,pv_anCDF_2,pv_emp_2,pv_emp_21,phase='Phase 2')
 
 # plots for phase 3
-plot(v_3,pv_anPDF_3,pv_emp_3,pv_emp_31,phase='Phase 3')
+plot(v_3,pv_anPDF_3,pv_anCDF_3,pv_emp_3,pv_emp_31,phase='Phase 3')
 
 # plots for pv total
 pv_total(pv_1,pv_2,phase='Total')
--- plots.py
+++ plots.py
@@ -2,18 +2,22 @@ import matplotlib.pyplot as plt
 # from sklearn import preprocessing
 # import seaborn as sns
 
-def plot(v,pv_anPDF,pv_emp_exp,pv_emp_log,phase):
-    plt.hist(pv_emp_exp,bins=100,density=1)
+def plot(v,pv_anPDF,pv_anCDF,pv_emp_exp,pv_emp_log,phase):
+    plt.hist(pv_emp_exp,bins=100,density=1,color='blue',alpha=0.6,label='Simulated PDF (exponential)')
     # sns.distplot(pv_emp, hist=True, kde=False, bins=100, norm_hist=True)
-    plt.plot(v,pv_anPDF,'r--')
-    # plt.hist(pv_emp_log,bins=100,density=1,color='green')
+    plt.plot(v,pv_anPDF,'r--',label='Analytical PDF (exponential)')
+    plt.plot(v,pv_anCDF,'k:',label='Analytical CDF (exponential)')
+    plt.hist(pv_emp_log,bins=100,density=1,color='green',alpha=0.7,label='Simulated PDF (log-norm)')
     plt.xlabel('Present Value '+ '('+phase+')')
     plt.ylabel('Frequency')
+    plt.legend(loc='best')
     plt.show()
 
 
 def pv_total(pv1,pv2,phase):
-    plt.hist(pv1,bins=100,density=1)
+    plt.hist(pv1,bins=100,density=1,color='blue',alpha=0.6,label='Simulated PDF (exponential)')
+    plt.hist(pv2,bins=100,density=1,color='green',alpha=0.7,label='Simulated PDF (log-norm)')
     plt.xlabel('Present Value '+ '('+phase+')')
     plt.ylabel('Frequency')
+    plt.legend(loc='best')
     plt.show()
